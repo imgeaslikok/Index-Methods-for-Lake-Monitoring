a700ac30cfb9d75530fcf9b94d1ca82111e626eb
imgeaslikok <imgeaslikok@gmail.com>
2019-11-12 17:08:04 +0300

    function for selecting ROI

---
+++ customWait.m
@@ -0,0 +1,16 @@
+function pos = customWait(hROI)
+% Listen for mouse clicks on the ROI
+l = addlistener(hROI,'ROIClicked',@clickCallback);
+% Block program execution
+uiwait;
+% Remove listener
+delete(l);
+% Return the current position
+pos = hROI.Position;
+close gcf;
+end
+function clickCallback(~,evt)
+if strcmp(evt.SelectionType,'double')
+uiresume;
+end
+end
